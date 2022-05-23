c14ad06f7b159dd21a97380d0de23432d2922721
Mark Conroy <mark@mark.ie>
2022-05-23 20:02:47 +0200

    initial commit

---
+++ js/localgov-microsites-colour-picker-fields.js
@@ -0,0 +1,18 @@
+(function colourPickerFieldsScript(Drupal) {
+  Drupal.behaviors.micrositeColourPickerFields = {
+    attach: function (context, settings) {
+      context = context || document;
+
+      const colourPickerFields = context.querySelectorAll('.colour-picker-field');
+      colourPickerFields.forEach(item => {
+        const text = item.querySelector('.colour-picker-field__text');
+        const picker = item.querySelector('.colour-picker-field__picker');
+        picker.addEventListener('change', (e) => {
+          let colour = e.target.value
+          text.value = colour;
+        })
+      })
+
+    },
+  };
+})(Drupal);
