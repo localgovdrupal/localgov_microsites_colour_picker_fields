d802075f09c0e984f89700fa4f6c58fcdac9a6ca
Mark Conroy <mark@mark.ie>
2022-06-01 10:29:28 +0200

    tidies up colour picker JS

--- js/localgov-microsites-colour-picker-fields.js
+++ js/localgov-microsites-colour-picker-fields.js
@@ -4,22 +4,28 @@
       context = context || document;
 
       const colourPickerFields = context.querySelectorAll('.colour-picker-field');
+
       colourPickerFields.forEach(item => {
         const text = item.querySelector('.colour-picker-field__text');
         const picker = item.querySelector('.colour-picker-field__picker');
-        picker.addEventListener('change', (e) => {
-          let colour = e.target.value
-          text.value = colour;
-        })
-        if (text.value) {
-          picker.value = text.value;
-        }
-        text.addEventListener('change', (e) => {
-          let colour = text.value
-          picker.value = colour;
-        })
-      })
 
+        function handleSetColours(item1 = text, item2 = picker) {
+          let colour = item1.value;
+          item2.value = colour;
+          text.style.border = `5px solid ${colour}`;
+        }
+
+        if (text.value) {
+          handleSetColours();
+        }
+        picker.addEventListener('input', (e) => {
+          handleSetColours(picker, text);
+        });
+        text.addEventListener('change', (e) => {
+          handleSetColours();
+        });
+        
+      })
     },
   };
 })(Drupal);
